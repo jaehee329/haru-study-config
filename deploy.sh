905197584aa07865d0d21c943c7f7604a3360a5f
Jaehee <77962265+jaehee329@users.noreply.github.com>
2024-03-03 23:13:12 +0900

    fix: 동기적으로 java 프로세스를 재시작하도록 변경

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
-PID=$(pgrep -f "$PORT" -u ubuntu)
-
-echo $PID
-
-if [ -z "$PID" ]; then
-  echo "포트 $PORT에서 실행 중인 프로세스가 없습니다."
-else
-  echo "포트 $PORT에서 실행 중인 프로세스 종료: $PID"
-  kill -9 "$PID"
-fi
-
-JAR_PATH=$(docker exec -it amazoncorretto17 find /app/deploy -name "*.jar")
-
-echo "run server"
-
-docker exec -it amazoncorretto17 nohup java -jar -Dspring.profiles.active=production -Dserver.port=$PORT $JAR_PATH &
+{
+    echo "stop container"
+    docker stop amazoncorretto17
+} && 
+{
+    echo "rerun container"
+    docker start amazoncorretto17
+} && 
+{
+    echo "container started, getting JAR_PATH..."
+    JAR_PATH=$(docker exec amazoncorretto17 find /app/deploy -name "*.jar" | tr -d '\r')
+    if [ -n "$JAR_PATH" ]; then
+        echo "run server"
+        docker exec -d amazoncorretto17 nohup java -jar -Dspring.profiles.active=production -Dserver.port=$PORT $JAR_PATH &
+    else
+        echo "Failed to get JAR_PATH."
+    fi
+}
